5e8e207e5495b9a4378941d07a95290e1ea558c6
suhyang1166 <yooyootea1166@gmail.com>
2024-02-12 00:11:50 +0900

    20240212

--- 01. 개인 프로젝트/script.js
+++ 01. 개인 프로젝트/script.js
@@ -1,6 +1,5 @@
-const phone = document.querySelector(".hello-content");
-const move = document.querySelector(".text-ani");
 const gnb = document.querySelector("#main-bar");
+const gototop = document.querySelector(".gototop");
 const like = document.querySelector(".like");
 const logo = document.querySelector("#logo");
 
@@ -10,15 +9,16 @@ console.log(logo);
 window.addEventListener("scroll", () => {
   if (window.scrollY > 680) {
     gnb.classList.add("active");
+    gototop.classList.add("active");
     logo.classList.add("active");
   } else {
     gnb.classList.remove("active");
+    gototop.classList.remove("active");
     logo.classList.remove("active");
   }
 });
 
 // mouse cursor
-
 const cursor = document.querySelector(".cursor");
 console.log(cursor);
 document.addEventListener("mousemove", (e) => {
@@ -26,40 +26,60 @@ document.addEventListener("mousemove", (e) => {
   cursor.style.top = e.clientY + "px";
 });
 
-// 스크롤
-// const observer = new IntersectionObserver((move) => {
-//   move.forEach((move) => {
-//     if (move.isIntersecting) {
-//       move.target.classList.add("click");
-//     } else {
-//       move.target.classList.remove("click");
-//     }
-//   });
-// });
-
-// move.forEach((move) => observer.observe(move));
-
 // 텍스트 타이핑
+const phone = document.querySelector(".hello-content");
 const textArr = ["H", "E", "L", "L", "O", ",", " ", "I", "'", "m"];
+const move = document.querySelector(".text-ani");
+let isClicked = false;
+
+function blink() {
+  move.classList.toggle("active");
+}
+
+phone.addEventListener("click", function (event) {
+  if (!isClicked) {
+    isClicked = true;
+    textAni(textArr.slice());
+    event.stopPropagation();
+
+    // a 태그 좌표로 이동하는 부분 추가
+    const targetId = document.querySelector("#third-move");
+    const targetElement = document.getElementById(targetId);
+
+    if (targetElement) {
+      targetElement.scrollIntoView({ behavior: "smooth" });
+    }
+  }
+});
 
 function textAni(randomArr) {
   if (randomArr.length > 0) {
-    move.textContent += randomArr.shift();
+    const char = randomArr.shift();
+    move.textContent += char;
     setTimeout(function () {
       textAni(randomArr);
     }, 300);
   }
 }
 
-textAni(textArr);
-
-// 커서 깜빡임
-function blink() {
-  move.classList.toggle("active");
-}
-setInterval(blink, 400);
-
 // 좋아요 heart
 like.addEventListener("click", () => {
   like.classList.toggle("active");
 });
+
+// jQuery
+
+// scrollTo
+// $("a").click(function (e) {
+//   $.scrollTo(this.hash || 0, 1200);
+//   e.preventDefault();
+// });
+
+// wow
+let wow = new WOW({
+  boxClass: "wow",
+  offset: 120,
+  mobile: false,
+  live: true,
+});
+wow.init();
